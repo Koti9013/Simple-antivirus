89ac1cfcb0d0f9dbf3ea54b06875e290d62a3448
Koti9013 <artemen000@gmail.com>
2026-01-09 21:22:57 +0200

    Finished localisation, releasing v.0.2

--- Simple antivirus.bat
+++ Simple antivirus.bat
@@ -1,13 +1,13 @@
 @echo off
-set "version=0.1"
+set "version=0.2"
 title Simple Antivirus v%version%
 chcp 65001 >nul
-echo [Система] Проверка обновлений...
+echo [System] Checking updates...
 
 curl -f -s -L "https://raw.githubusercontent.com/Koti9013/Simple-antivirus/refs/heads/main/version.txt" -o "%temp%\ver.txt"
 
 if %errorlevel% neq 0 (
-    echo [!] Сервер обновлений недоступен. Работа в офлайн-режиме.
+    echo [!] Server isn't connected. Working offline.
     timeout /t 2 >nul
     goto scan
 )
@@ -16,15 +16,15 @@ set /p remote_ver=<%temp%\ver.txt
 set "remote_ver=%remote_ver: =%"
 
 if "%remote_ver%"=="%version%" (
-    echo [OK] У вас актуальная версия.
+    echo [OK] you have an actual version.
     timeout /t 2 >nul
     goto scan
 )
 
-echo [!] Найдена новая версия: %remote_ver%
-echo [!] Загрузка обновления...
+echo [!] New version foung: %remote_ver%
+echo [!] Uploading the new version...
 curl -f -s -L "https://raw.githubusercontent.com/Koti9013/Simple-antivirus/refs/heads/main/Simple%20antivirus.bat" -o "%~f0"
-echo [OK] Обновление завершено. Перезапустите программу!
+echo [OK] Antivirus updated. Restart it, please!
 pause
 exit
 
@@ -33,7 +33,7 @@ cls
 echo ==========================================
 echo       Simple Antivirus v%version%
 echo ==========================================
-echo [!] WARNING: need administrator rights to work normally.
+echo [!] WARNING: need administrator rights to work property.
 echo.
 
 chcp 65001 >nul
